547c4d217a635c12a9a0fce819987996631be2c3
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2024-08-04 14:52:40 -0300

    fix(program): lambda can't read from App.config

--- ChauMundo/LambdaEntryPoint.cs
+++ ChauMundo/LambdaEntryPoint.cs
@@ -1,12 +1,20 @@
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.AspNetCoreServer;
+using Amazon.Lambda.AspNetCoreServer.Internal;
+using Amazon.Lambda.Core;
 
 namespace ChauMundo
 {
-    public class LambdaEntryPoint : APIGatewayProxyFunction
+    public class LambdaEntryPoint : APIGatewayHttpApiV2ProxyFunction
     {
         protected override void Init(IWebHostBuilder builder)
         {
             builder
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .UseStartup<Startup>();
         }
     }
